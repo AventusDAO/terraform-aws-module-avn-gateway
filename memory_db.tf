936e48bdb4ea474623b680d147c4c1840eaa81e7
Rui <m.rui.s.magalhaes@gmail.com>
2023-07-20 19:06:04 +0100

    fix typos memoryDB

--- memory_db.tf
+++ memory_db.tf
@@ -6,22 +6,22 @@ module "memory_db" {
   version = "v1.1.2"
 
   # Cluster
-  name        = lookup(var.memory_db, "override_name", var.name)
-  description = lookup(var.memory_db, "description", "Gateway MemoryDB cluster (redis)")
+  name        = coalesce(var.memory_db.override_name, var.name)
+  description = var.memory_db.description
 
-  engine_version             = lookup(var.memory_db, "engine_version", "6.2")
-  auto_minor_version_upgrade = lookup(var.memory_db, "auto_minor_version_upgrade", false)
-  node_type                  = lookup(var.memory_db, "node_type", "db.t4g.small")
-  num_shards                 = lookup(var.memory_db, "num_shards", 1)
-  num_replicas_per_shard     = lookup(var.memory_db, "num_replicas_per_shard", 3)
+  engine_version             = var.memory_db.engine_version
+  auto_minor_version_upgrade = var.memory_db.auto_minor_version_upgrade
+  node_type                  = var.memory_db.node_type
+  num_shards                 = var.memory_db.num_shards
+  num_replicas_per_shard     = var.memory_db.num_replicas_per_shard
 
-  port                     = lookup(var.memory_db, "port", 6379)
-  tls_enabled              = lookup(var.memory_db, "tls_enabled", false)
+  port                     = var.memory_db.port
+  tls_enabled              = var.memory_db.tls_enabled
   security_group_ids       = [module.sg_memorydb.security_group_id]
-  maintenance_window       = lookup(var.memory_db, "maintenance_window", "tue:09:00-tue:10:00")
+  maintenance_window       = var.memory_db.maintenance_window
   sns_topic_arn            = var.memory_db.sns_topic_arn
-  snapshot_retention_limit = lookup(var.memory_db, "snapshot_retention_limit", 14)
-  snapshot_window          = lookup(var.memory_db, "snapshot_window", "06:00-07:00")
+  snapshot_retention_limit = var.memory_db.snapshot_retention_limit
+  snapshot_window          = var.memory_db.snapshot_window
 
   #TODO: revisit Users and ACL - for now avn-connector doesn't allow configuring a user to connect to redis (it uses the default one)
   # Should create an admin and connector (with limited access) user and add it to a specific ACL.
@@ -34,14 +34,14 @@ module "memory_db" {
   acl_name   = "open-access"
 
   # Parameter group
-  create_parameter_group = lookup(var.memory_db, "create_parameter_group", false)
-  parameter_group_name   = lookup(var.memory_db, "parameter_group_name", "default.memorydb-redis6")
+  create_parameter_group = var.memory_db.create_parameter_group
+  parameter_group_name   = var.memory_db.parameter_group_name
 
   # Subnet group
-  subnet_group_name        = lookup(var.memory_db, "subnet_group_name", "${lookup(var.memory_db, "override_name", var.name)}-subnet-group")
-  subnet_group_description = lookup(var.memory_db, "subnet_group_description", "${lookup(var.memory_db, "override_name", var.name)} MemoryDB subnet group")
+  subnet_group_name        = coalesce(var.memory_db.subnet_group_name, "${coalesce(var.memory_db.override_name, var.name)}-subnet-group")
+  subnet_group_description = coalesce(var.memory_db.subnet_group_description, "${coalesce(var.memory_db.override_name, var.name)} MemoryDB subnet group")
   subnet_ids               = var.memory_db.subnet_ids
-  subnet_group_tags        = lookup(var.memory_db, "subnet_group_tags", {})
+  subnet_group_tags        = var.memory_db.subnet_group_tags
 
-  tags = merge(lookup(var.memory_db, "tags", {}), { Name = lookup(var.memory_db, "override_name", var.name) })
+  tags = merge(var.memory_db.tags, { Name = coalesce(var.memory_db.override_name, var.name) })
 }
