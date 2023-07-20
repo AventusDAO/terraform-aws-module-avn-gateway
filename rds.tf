583cdb2c06e35674d148b7c7ac8f22e66225a067
Rui <m.rui.s.magalhaes@gmail.com>
2023-07-20 18:42:18 +0100

    fix typos

--- rds.tf
+++ rds.tf
@@ -5,7 +5,7 @@ module "rds" {
   source  = "terraform-aws-modules/rds/aws"
   version = "6.1.0"
 
-  identifier = "aaaa"
+  identifier = lookup(var.rds, "override_name", var.name)
 
   # All available versions: https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/CHAP_PostgreSQL.html#PostgreSQL.Concepts
   engine                     = lookup(var.rds, "engine", "postgres")
