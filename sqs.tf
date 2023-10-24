16b68b3d6484965e41089ed3e044d0cf9d4b7fac
Rui <m.rui.s.magalhaes@gmail.com>
2023-10-24 20:42:46 +0100

    sqs migration

--- sqs.tf
+++ sqs.tf
@@ -26,7 +26,7 @@ module "gateway_sqs_queues_alarms" {
   version = "4.3.0"
 
   alarm_name          = replace("${each.key}-dlq-alarm", "_", "-")
-  alarm_description   = coalesce(var.sqs.alarm.alarm_description, "Warning: DLQ queue [${each.key}] has more than 20 messages in the queue. Please investigate and take appropriate actions to avoid service disruption.")
+  alarm_description   = coalesce(var.sqs.alarm.alarm_description, "Warning: DLQ queue [${each.key}-dlq.fifo] has more than 20 messages in the queue. Please investigate and take appropriate actions to avoid service disruption.")
   comparison_operator = var.sqs.alarm.comparison_operator
   evaluation_periods  = var.sqs.alarm.evaluation_periods
   threshold           = var.sqs.alarm.threshold
