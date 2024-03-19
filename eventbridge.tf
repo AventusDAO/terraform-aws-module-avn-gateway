3bc114a459b5f8151502cb4f6b8476ca1f9704f6
Vucomir Ianculov <vukomir@ianculov.ro>
2024-03-19 13:12:21 +0200

    DEVOPS-720 added eventbridge rules to terraform

--- eventbridge.tf
+++ eventbridge.tf
@@ -19,13 +19,13 @@ module "eventbridge" {
     tx_status_update_handler = [
       {
         name = "tx_status_update_handler"
-        arn  = module.lambdas["tx_status_update_handler"].lambda_function_invoke_arn
+        arn  = module.lambdas["tx_status_update_handler"].lambda_function_arn
       }
     ]
     lift_processing_handler = [
       {
         name = "lift_processing_handler"
-        arn  = module.lambdas["lift_processing_handler"].lambda_function_invoke_arn
+        arn  = module.lambdas["lift_processing_handler"].lambda_function_arn
       }
     ]
   }
