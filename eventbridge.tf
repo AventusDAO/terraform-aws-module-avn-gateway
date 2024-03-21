4197164aa258bd6c4f62e449593677085e22b8e6
Vucomir Ianculov <vukomir@ianculov.ro>
2024-03-21 13:39:55 +0200

    fix: schedule_expression

--- eventbridge.tf
+++ eventbridge.tf
@@ -6,12 +6,12 @@ module "eventbridge" {
 
   rules = {
     tx_status_update_handler = {
-      description         = "trigger lambda tx_status_update_handler"
-      schedule_expression = "rate(1 minute)"
+      description         = "Job to update transaction state by checking block indexer resolve-pending-transactions"
+      schedule_expression = "rate(1 minutes)"
     }
     lift_processing_handler = {
-      description         = "trigger lambda lift_processing_handler"
-      schedule_expression = "rate(10 minute)"
+      description         = "process-lifts trigger lambda lift_processing_handler"
+      schedule_expression = "rate(3 minutes)"
     }
   }
 
