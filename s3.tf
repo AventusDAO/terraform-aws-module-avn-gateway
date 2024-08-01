d75870fff94d1b071259c05aca9e5d6a94c3384c
Vucomir Ianculov <2032768+vukomir@users.noreply.github.com>
2024-08-01 12:24:37 +0300

    DEVOPS-878 created S3 bucket for voting #minor (#56)
    1. added S3 bucket for `vote handler`
    2. added a condition to create S3 bucket
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ s3.tf
@@ -0,0 +1,14 @@
+module "s3_vote_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+
+  create_bucket            = var.lambdas.vote_handler.vote_bucket_create
+  bucket                   = var.lambdas.vote_handler.vote_bucket
+  acl                      = "private"
+  control_object_ownership = true
+  object_ownership         = "BucketOwnerPreferred"
+
+  versioning = {
+    enabled = true
+  }
+}
