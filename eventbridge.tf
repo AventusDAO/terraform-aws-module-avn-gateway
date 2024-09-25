0be73ba4ea3cfb70dd6d03a640f4177427d267a7
Vucomir Ianculov <vukomir@ianculov.ro>
2024-09-25 12:10:22 +0300

    DEVOPS-956 Update AvN Gateway terraform module to disable eventbridge rules

--- eventbridge.tf
+++ eventbridge.tf
@@ -8,10 +8,12 @@ module "eventbridge" {
     tx_status_update_handler = {
       description         = "Job to update transaction state by checking block indexer resolve-pending-transactions"
       schedule_expression = "rate(1 minute)"
+      state               = var.eventbridge_rules.tx_status_update_state
     }
     lift_processing_handler = {
       description         = "process-lifts trigger lambda lift_processing_handler"
       schedule_expression = "rate(3 minutes)"
+      state               = var.eventbridge_rules.tx_status_update_state
     }
   }
 
