8959f481b969e333902daf082786b6e702aee9ca
Vucomir Ianculov <vukomir@ianculov.ro>
2025-01-16 10:47:19 +0200

    DEVOPS-1012 Configure AvN Gateway to work on aventus.io domain

--- cognito.tf
+++ cognito.tf
@@ -28,7 +28,7 @@ resource "aws_cognito_user_pool" "admin_portal" {
       Your username is <b>{username}</b> and temporary password is <b>{####}</b>
       <p>To log in, browse to <span style="color: #5100ff;">https://${local.domain_admin_portal}</span></p> <p>You will be prompted to change your password when you first log in, this password will expire in 1 day.</p>
       <br/><br/> <br/>
-      <h4>Powered by Aventus (www.aventus.network)</h4>
+      <h4>Powered by Aventus (www.aventus.io)</h4>
     </div>
 EOT
       email_subject = "${title(replace(var.name, "-", " "))} registration - your temporary password"
--- route53.tf
+++ route53.tf
@@ -4,7 +4,7 @@
 resource "aws_route53_record" "admin_portal" {
   count = var.cognito.create_dns_record ? 1 : 0
 
-  zone_id         = var.route53_zone_id
+  zone_id         = var.cognito.route53_zone_id
   name            = aws_cognito_user_pool_domain.admin_portal.domain
   type            = "A"
   allow_overwrite = true
@@ -17,7 +17,44 @@ resource "aws_route53_record" "admin_portal" {
   }
 }
 
-moved {
-  from = aws_route53_record.admin_portal
-  to   = aws_route53_record.admin_portal[0]
+#
+# API Gateway custom record
+#
+resource "aws_apigatewayv2_domain_name" "this" {
+  for_each = var.api_gateway.domains
+
+  domain_name = each.value.domain_name
+
+  domain_name_configuration {
+    certificate_arn = each.value.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+
+  tags = var.api_gateway.tags
+}
+
+resource "aws_apigatewayv2_api_mapping" "this" {
+  for_each = var.api_gateway.domains
+
+  api_id      = module.api_gateway.api_id
+  domain_name = aws_apigatewayv2_domain_name.this[each.key].id
+  stage       = module.api_gateway.stage_id
+}
+
+resource "aws_route53_record" "api_gateway" {
+  for_each = {
+    for key, value in var.api_gateway.domains : key => value
+    if value.create_dns_record
+  }
+
+  zone_id = each.value.route53_zone_id
+  name    = each.value.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.this[each.key].domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.this[each.key].domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
 }
