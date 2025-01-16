aff1e6ff8deeb5289e5382779de1b04115baffd5
Vucomir Ianculov <vukomir@ianculov.ro>
2025-01-16 11:41:22 +0200

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -9,10 +9,10 @@ output "dns_settings" {
   description = "DNS Settings for API Gateway and Admin Portal"
   value = tomap({
     api_gateway = {
-      for k, v in aws_apigatewayv2_domain_name.api_gateway : k => {
-        name               = try(var.api_gateway[k].domain_name, null)
-        hosted_zone_id     = try(v.domain_name_configuration[0].hosted_zone_id, null)
-        target_domain_name = try(v.domain_name_configuration[0].target_domain_name, null)
+      for k, v in var.api_gateway.domains : k => {
+        name               = try(v.domain_name, null)
+        hosted_zone_id     = try(aws_apigatewayv2_domain_name[k].domain_name_configuration[0].hosted_zone_id, null)
+        target_domain_name = try(aws_apigatewayv2_domain_name[k].domain_name_configuration[0].target_domain_name, null)
       }
     }
     admin_portal = {
